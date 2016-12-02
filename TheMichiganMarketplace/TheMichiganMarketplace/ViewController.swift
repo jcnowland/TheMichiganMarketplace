cd4da4d667701d2d793e9917083ec7c0816e7a72
John Nowland <jnowl@umich.edu>
2016-12-01 21:22:40 -0500

    Starting Project
    First project LFG

---
+++ TheMichiganMarketplace/TheMichiganMarketplace/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheMichiganMarketplace
+//
+//  Created by John Nowland on 12/1/16.
+//  Copyright © 2016 John Nowland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
